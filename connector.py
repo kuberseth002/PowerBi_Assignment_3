3deb338b3c4e938a03f3410ad013a2c71629fc34
kuberseth002 <kuber0804.be21@chitkara.edu.in>
2025-03-28 22:19:38 +0530

    [kuber]Add: modified connection file

--- connector.py
+++ connector.py
@@ -6,10 +6,10 @@ conn = mysql.connector.connect(
     host='localhost',
     user='root',
     password='Kuberseth#@0002',
-    database='your_database'   
+    database='assignment3'   
 )
 
-query = "SELECT * FROM your_table"  
+query = "SELECT * FROM user_behavior_dataset"  
 
 
 df = pd.read_sql(query, conn)
